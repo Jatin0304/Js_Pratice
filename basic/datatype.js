f1c16f43c61bc692fe1f5e861fa545e63bd840c9
Jatin0304 <cg033224@gmail.com>
2024-08-04 10:23:29 +0000

    datatypes

--- basic/datatype.js
+++ basic/datatype.js
@@ -2,3 +2,20 @@
 
 //alert("3+3") // We are using nodejs. not browser 
 
+let name = "jatin"
+let age = 23
+let isLoggedIn = false
+let state = null
+
+// primitive datatypes
+/* number => 2 to pwer 53
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined =>
+symbol => unique
+*/
+
+// object 
+console.log(typeof age );
