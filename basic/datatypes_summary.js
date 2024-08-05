b8198fedffcfc9490f9d956946fee1cf7af14c0f
Jatin0304 <cg033224@gmail.com>
2024-08-05 10:41:48 +0000

    datatypes_summary

--- basic/datatypes_summary.js
+++ basic/datatypes_summary.js
@@ -14,8 +14,21 @@ const anotherId = Symbol('123')
 
 console.log(id === anotherId);
 
+//const bigNumber = 232434343434334434n
 
 
 // Reference (Non primitive)
 
 // Array, Objects, functions
+
+const heros =["shaktiman","naagraj","doga"]
+let myObj= {
+    name: "jatin",
+    age: 21
+};
+
+const myFunction = function (){
+    console.log("hello world")
+}
+
+console.log(typeof myFunction);
