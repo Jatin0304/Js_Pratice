d4618751908b866ad8cf822e1aadd87c22c20607
Jatin0304 <cg033224@gmail.com>
2024-08-06 06:11:53 +0000

    string

---
+++ basic/string.js
@@ -0,0 +1,19 @@
+const name = "Jatin"
+const repoCount = 20
+
+//console.log(name + repoCount + "repostry");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName  = new String('Jatin');
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+
